1a008253f1501589809037d7107447121b8035ed
Rohit Arya <nandan.rohitarya@gmail.com>
2025-06-06 13:24:07 +0530

    Added API Gateway using ocelot

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,18 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
+app.MapControllers();
+app.UseOcelot().Wait();
+
+app.Run();
